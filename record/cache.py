36a16253fbc79ac8487d0ea3ef53e1040604df95
Charles Qi <qiyichuan@gmail.com>
2018-03-09 15:58:26 +0800

    test

--- record/cache.py
+++ record/cache.py
@@ -4,7 +4,7 @@ from shutil import rmtree
 
 class Cache:
 
-    cache_path = ''
+    cache_path = '/root/ddns'
     db_prefix = '.cache'
     key_prefix = ''
     key_suffix = ''
@@ -31,10 +31,11 @@ class Cache:
         self.key_suffix = key_suffix
 
     def set_cache_path(self, cache_path):
-        self.cache_path = cache_path + '/'
+        self.cache_path = cache_path
 
     def select(self, db):
         self.db = db
+        self.cache_path += self.db_prefix + str(self.db)
 
     def set(self, key, value):
         self.__check_path()
