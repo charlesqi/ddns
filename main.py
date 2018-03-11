cf246bc3d13e47991e8551fcdd3fad1bb653d9f6
Charles Qi <qiyichuan@gmail.com>
2018-03-12 07:03:32 +0800

    Fixed the working path bug

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+import os
 from record import record
 
+path = '/root/ddns/'
+os.chdir(path)
 config_file = 'settings.json'
 
 task = record.Update(config_file)
