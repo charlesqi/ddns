1b96cc903d99719667b430c6e00d2d496a5d167b
Charles Qi <qiyichuan@gmail.com>
2018-03-18 10:01:34 +0800

    Modified the working path selection

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 from record import record
 
 path = '/root/ddns/'
-os.chdir(path)
+if os.path.exists(path):
+    os.chdir(path)
 config_file = 'settings.json'
 
 task = record.Update(config_file)
